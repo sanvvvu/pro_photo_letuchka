b3dc58744c96cecb1ec0e2d9d1c32c5e9f418b82
sanvvu <kekovaekaterina@yandex.ru>
2026-05-01 01:30:06 +0300

    rework ml model no static

--- backend/services/ml_model.py
+++ backend/services/ml_model.py
@@ -1,30 +1,57 @@
-# backend/services/ml_model.py
-import numpy as np
+import os
 import cv2
+import numpy as np
 from sklearn.ensemble import RandomForestClassifier
+import joblib
 
-X = np.array([
-    [100000, 120, 30],
-    [200000, 50, 10]
-])
-y = np.array([0, 1])
-
-model = RandomForestClassifier()
-model.fit(X, y)
+MODEL_PATH = "models/model.pkl"
 
 
-def predict_image(path):
+def extract_features(path):
     img = cv2.imread(path)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    feat = np.array([
+    return [
         img.size,
         np.mean(gray),
-        np.std(gray)
-    ]).reshape(1, -1)
+        np.std(gray),
+        np.var(gray)
+    ]
 
-    pred = model.predict(feat)[0]
 
-    return {
-        "edited": bool(pred)
-    }
+def train_model(dataset_path="dataset"):
+    X, y = [], []
+
+    for file in os.listdir(dataset_path):
+        path = os.path.join(dataset_path, file)
+
+        features = extract_features(path)
+
+        if file.startswith("orig"):
+            label = 0
+        else:
+            label = 1
+
+        X.append(features)
+        y.append(label)
+
+    model = RandomForestClassifier(n_estimators=100)
+    model.fit(X, y)
+
+    joblib.dump(model, MODEL_PATH)
+
+
+def load_model():
+    if not os.path.exists(MODEL_PATH):
+        train_model()
+    return joblib.load(MODEL_PATH)
+
+
+model = load_model()
+
+
+def predict_image(path):
+    features = np.array(extract_features(path)).reshape(1, -1)
+    pred = model.predict(features)[0]
+
+    return {"edited": bool(pred)}
