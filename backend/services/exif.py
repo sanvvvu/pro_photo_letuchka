7a3260461006b65622320e4d066416ed4ca73aec
sanvvu <kekovaekaterina@yandex.ru>
2026-05-01 01:05:58 +0300

    update full exif

--- backend/services/exif.py
+++ backend/services/exif.py
@@ -4,14 +4,25 @@ import piexif
 def get_exif_data(path):
     img = Image.open(path)
     if "exif" not in img.info:
-        return {"exif": None}
-    return piexif.load(img.info["exif"])
+        return {}
+    exif = piexif.load(img.info["exif"])
+
+    readable = {}
+    for ifd in exif:
+        readable[ifd] = {}
+        for tag, val in exif[ifd].items():
+            readable[ifd][str(tag)] = str(val)
+    return readable
 
 
-def edit_simple_exif(input_path, output_path, model_name):
-    img = Image.open(input_path)
+def update_exif(path, new_exif, out):
+    img = Image.open(path)
 
-    exif_dict = {"0th": {piexif.ImageIFD.Model: model_name.encode()}}
+    exif_dict = {"0th": {}, "Exif": {}, "GPS": {}}
+
+    for ifd in new_exif:
+        for tag, val in new_exif[ifd].items():
+            exif_dict[ifd][int(tag)] = val.encode()
+
     exif_bytes = piexif.dump(exif_dict)
-
-    img.save(output_path, exif=exif_bytes)
+    img.save(out, exif=exif_bytes)
