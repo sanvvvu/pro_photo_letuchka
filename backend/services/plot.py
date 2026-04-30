6dac517e96a3f2d520edf495fa7a6dc627ed5bb4
sanvvu <kekovaekaterina@yandex.ru>
2026-05-01 01:30:46 +0300

    add plot.py

---
+++ backend/services/plot.py
@@ -0,0 +1,15 @@
+import cv2
+import matplotlib.pyplot as plt
+import os
+
+def histogram_plot(path):
+    img = cv2.imread(path, 0)
+
+    plt.figure()
+    plt.hist(img.ravel(), bins=256)
+
+    out = "uploads/hist.png"
+    plt.savefig(out)
+    plt.close()
+
+    return "hist.png"
